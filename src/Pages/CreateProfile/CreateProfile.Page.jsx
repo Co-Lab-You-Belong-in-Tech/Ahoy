d31ec5e89069e09b0375812cf7661ed59504f1d3
Mostafa Ahangarha <ahangarha@riseup.net>
2022-10-23 23:55:57 +0300

    Change navigation functions

--- src/Pages/CreateProfile/CreateProfile.Page.jsx
+++ src/Pages/CreateProfile/CreateProfile.Page.jsx
@@ -6,32 +6,20 @@ import ProfileComplete from './ProfileComplete/ProfileComplete.component';
 
 const CreateProfile = () => {
   const [pageNumber, setPageNumber] = useState(0);
-  const goToNextPage = (value) => {
-    (
-      setPageNumber((pageNumber) => pageNumber + value)
-    );
+  const goToNextPage = () => {
+    setPageNumber(() => pageNumber + 1);
   };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(data);
-  // };
-
-  // const tabs = [<UserDetails pageNumber={pageNumber} goToNextPage={goToNextPage} key={1} />,
-  //   <UserGender pageNumber={pageNumber} goToNextPage={goToNextPage} key={2} />,
-  //   <UserImage
-  //     key={3}
-  //     pageNumber={pageNumber}
-  //     goToNextPage={goToNextPage}
-  //     selectedFile={selectedFile}
-  //     setSelectedFile={setSelectedFile}
-  //   />,
-  //   <ProfileComplete key={4} />,
-  // ];
+  const goToPreviousPage = () => {
+    setPageNumber(() => pageNumber - 1);
+  };
 
   return (
     <section className="create-profile-page">
-      <ProfileHeader goToNextPage={goToNextPage} pageNumber={pageNumber} />
+      <ProfileHeader
+        goToNextPage={goToNextPage}
+        goToPreviousPage={goToPreviousPage}
+        pageNumber={pageNumber}
+      />
       <main>
         {pageNumber === 0 && <UserDetails goToNextPage={goToNextPage} />}
         {pageNumber === 1 && (
--- src/Pages/CreateProfile/ProfileHeader/ProfileHeader.component.jsx
+++ src/Pages/CreateProfile/ProfileHeader/ProfileHeader.component.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { IoIosArrowBack } from 'react-icons/io';
 
-const ProfileHeader = ({ goToNextPage, pageNumber }) => (
+const ProfileHeader = ({ goToNextPage, goToPreviousPage, pageNumber }) => (
   (
     <header>
       <div className="back-btn-container">
-        <button type="button" className={pageNumber === 0 ? 'back-btn disable' : 'back-btn'} onClick={() => goToNextPage(-1)}>
+        <button type="button" className={pageNumber === 0 ? 'back-btn disable' : 'back-btn'} onClick={() => goToPreviousPage()}>
           <IoIosArrowBack />
         </button>
       </div>
-      <button type="button" className={pageNumber === 1 ? 'skip-btn disable' : 'skip-btn'} onClick={() => goToNextPage(1)}>skip</button>
+      <button type="button" className={pageNumber === 2 ? 'skip-btn disable' : 'skip-btn'} onClick={() => goToNextPage()}>skip</button>
     </header>
   )
 );
 
 ProfileHeader.propTypes = {
   goToNextPage: PropTypes.func,
+  goToPreviousPage: PropTypes.func,
   pageNumber: PropTypes.number,
 };
 
